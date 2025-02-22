236ec44d2f8660d0d46ac872ceec2a507d75f702
Bronya <3092179918@qq.com>
2025-02-22 22:36:51 +0800

    config

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
+import "C"
 import (
-    "fmt"
-    "time"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 )
 
+type Config struct {
+	Port int    `json:"port"`
+	Env  string `json:"env"`
+}
+
+func LoadConfig(filename string) (Config, error) {
+	var config Config
+	file, err := os.Open(filename)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	return config, err
+}
+
 func main() {
-    getTime()
+	config, err := LoadConfig("config.json")
+	if err != nil {
+		panic(err)
+	}
+	println("Port:", config.Port, "Env:", config.Env)
 }
 func getTime() {
-    t := time.Now()
-    fmt.Println(t)
+	t := time.Now()
+	fmt.Println(t)
 }

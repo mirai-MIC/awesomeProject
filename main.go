dca0a116345b03a2934942aa6079b2b5403c5cdc
Dr-SummerFlower <1721807170@qq.com>
2025-02-22 22:04:21 +0800

    002

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	getTime()
+    getTime()
 }
 func getTime() {
-	t := time.Now()
-	fmt.Println(t)
+    t := time.Now()
+    fmt.Println(t)
 }
